04a15834383131bc79b89cd77d192453c28896db
swostik shrestha <slokk03@gmail.com>
2022-03-08 15:18:39 +0200

    database connection created

---
+++ db.py
@@ -0,0 +1,61 @@
+#library to connect to postgresql
+import psycopg2
+
+try :
+    conn = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="password",
+    
+    )
+except Exception:
+    print('Database not connected ', type(Exception))
+else:
+
+    conn.autocommit = True
+    print("connection")
+    cur = conn.cursor()
+    #listing all the database that already exist
+    cur.execute("SELECT datname FROM pg_Database;")
+    list_database = cur.fetchall()
+    #checking if the database that we want to create already exists
+    if ("barona",) in list_database:
+        print("barona database already exists")
+    else:
+        sql = '''CREATE database barona'''
+        cur.execute(sql)
+        print("successfully created")
+finally:
+    conn.close()
+
+#creating schema
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname="barona",
+        user="postgres",
+        password="password",
+    
+    )
+except Exception as e:
+    print("Database not connected")
+else:
+    print("Database connected")
+    cur=conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS CONTACTS")
+    sql = ''' CREATE TABLE CONTACTS(
+        index INTEGER PRIMARY KEY,
+        name VARCHAR(50),
+        phone VARCHAR(25),
+        email VARCHAR(75),
+        id VARCHAR(20),
+        country VARCHAR(50) )'''
+    try:
+        cur.execute(sql)
+    except Exception:
+        print("please check sql query")
+    else:
+        conn.commit()
+        print("table created")
+finally:
+    conn.close()
